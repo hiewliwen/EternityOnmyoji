e72f40d0bb83e1fa9133414a85b9a9b7738ddf1d
Hiew Li Wen <retlif.wen@hotmail.com>
2019-08-06 17:54:05 +0800

    Added function to change bot's presence every 5 mins.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import asyncio
 import os
+import random
 
+import discord
 from discord.ext import commands
 
 import CONFIG
 
 bot = commands.Bot(command_prefix=['.'])
-# CHANNEL_ID = 607829309305913355 #dummy_server
 CHANNEL_ID = 607838204468658188  # Eternity Onmyoji
-
+PR_STATUES = CONFIG.PR_STATUES
 
 @bot.check
 async def check_commands(ctx):
@@ -33,6 +35,14 @@ async def on_command_error(ctx, error):
         raise error
 
 
+async def change_pr():
+    await bot.wait_until_ready()
+
+    while not bot.is_closed():
+        status = random.choice(PR_STATUES)
+        await bot.change_presence(activity=discord.Game(status))
+        await asyncio.sleep(300)
+
 for cog in os.listdir('./cogs'):
     if cog.endswith('.py') and not cog.startswith('_'):
         try:
@@ -42,4 +52,5 @@ for cog in os.listdir('./cogs'):
             print(f'{cog} cannot be loaded.')
             raise e
 
+bot.loop.create_task(change_pr())
 bot.run(CONFIG.DISCORD_TOKEN)
