f258c10b80474729d806d51f49f70b3744fe5266
Hiew Li Wen <retlif.wen@hotmail.com>
2019-08-07 21:12:46 +0800

    Added shikigami images downloader helper function.

---
+++ helpers/bountylocationsdatabase.py
@@ -0,0 +1,40 @@
+import csv
+
+import sqlite3
+
+BOUNTY_LOCATION_CSV = 'bounty_locations.csv'
+BOUNTY_LOCATION_DB = 'eternity.db'
+SECRETS_KEYWORDS = ['wraith', 'umbrella', 'summer', 'shshio', 'aoandon', 'secret', 'maple', 'river', 'storm']
+
+conn = sqlite3.connect(BOUNTY_LOCATION_DB)
+cur = conn.cursor()
+
+
+def csv_to_db(csv_file):
+    """
+    Save CSV to SQLite DB.
+    **REMEMBER TO USE THE EDITED CSV.**
+    :param csv_file: (str) File name (with extension) of the CSV file to be saved.
+    :return: None
+    """
+    with conn:
+        cur.execute("DROP TABLE IF EXISTS bounty_locations")
+        cur.execute("""CREATE TABLE bounty_locations (
+                        "shikigami_name" TEXT NOT NULL COLLATE NOCASE,
+                        "shikigami_image" TEXT NOT NULL,
+                        "mystery_clues" TEXT COLLATE NOCASE,
+                        "chapters" TEXT,
+                        "secrets" TEXT,
+                        "souls" TEXT,
+                        "encounters" TEXT,
+                        "others" TEXT);""")
+
+    with open(csv_file, 'r', newline='\n', encoding='utf-8') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader, None)  # Skip header
+        for field in reader:
+            with conn:
+                cur.execute("INSERT INTO bounty_locations VALUES (?, ?, ?, ?, ?, ?, ?, ?)", field, )
+
+
+csv_to_db(BOUNTY_LOCATION_CSV)
---
+++ helpers/image_downloader.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+import requests
+
+BOUNTY_LOCATION_DB = '../eternity.db'
+conn = sqlite3.connect(BOUNTY_LOCATION_DB)
+cur = conn.cursor()
+
+
+def fetch_image_links():
+    cur.execute("SELECT shikigami_image FROM bounty_locations")
+    return cur.fetchall()
+
+
+image_links = [link[0] for link in fetch_image_links()]
+
+if not image_links:
+    print('No links fetched.')
+else:
+    for link in image_links:
+        filename = link.split('/')[-1]
+        r = requests.get(link, allow_redirects=True)
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+        print(f'Downloaded {filename}')
